2dae29ad243f5055f428ceeb48d0e450fe080026
douglasrc <douglasr.rstr42@gmail.com>
2021-11-09 22:59:40 -0300

    criação primeiro componente header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Header() {
+  return (
+    <header>
+      <h3>Como criar sites: HTML e CSS</h3>
+      <p>
+        HTML blá blá blá <br /> etc etc etc
+      </p>
+    </header>
+  );
+}
